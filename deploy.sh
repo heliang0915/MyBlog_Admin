1bb64ced4fb60ed7e87f082606eb0a39c80d00e2
heliang <heliang@duyao001.com>
2018-05-31 13:37:24 +0800

    提交

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,6 @@
 cp -R /Users/heliang/work/web/frcore/blog/MyBlog_Admin/dist/* /Users/heliang/work/web/frcore/blog/BlogAdminSimple/dist
 cd /Users/heliang/work/web/frcore/blog/BlogAdminSimple
-git pull
+git pull git@github.com:heliang0915/BlogAdminSimple.git
 git add .
 git commit -m "提交"
 git push origin master
