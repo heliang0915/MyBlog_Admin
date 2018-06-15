0cdbc3c7212d9ebe2593fd6bfc376d5614c2c1cb
heliang <heliang@duyao001.com>
2018-06-15 15:27:11 +0800

    update

---
+++ deploy.sh
@@ -0,0 +1,5 @@
+cp -R /Users/hotread/work/learn/MyBlog_Admin/dist/* /Users/hotread/work/learn/BlogAdminSimple/dist
+cd /Users/hotread/work/learn/BlogAdminSimple
+git pull git@github.com:heliang0915/BlogAdminSimple.git
+git add .
+git commit -m "提交"
